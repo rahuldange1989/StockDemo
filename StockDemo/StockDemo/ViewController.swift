02efa0ff57070c44dc8aa7b52041b03507f05241
Rahul Dange <rahul.dange@globant.com>
2021-03-26 01:29:49 +0530

    Created xcode project.

---
+++ StockDemo/StockDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockDemo
+//
+//  Created by Rahul Dange on 26/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
