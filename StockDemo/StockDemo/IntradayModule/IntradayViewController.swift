d8dab5bdd6ac69c326f23d37dbc22e11a1d94ef2
Rahul Dange <rahul.dange@globant.com>
2021-03-29 11:49:37 +0530

    created screen design for Intraday screen

--- StockDemo/StockDemo/IntradayModule/IntradayViewController.swift
+++ StockDemo/StockDemo/IntradayModule/IntradayViewController.swift
@@ -9,10 +9,79 @@ import UIKit
 
 class IntradayViewController: UIViewController {
 
+    @IBOutlet weak var intradayTableView: UITableView!
+    @IBOutlet weak var getInfoBtn: UIButton!
+    @IBOutlet weak var noDataLabel: UILabel!
+    @IBOutlet weak var symbolTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setupUI()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /// hide keyboard when user touches outside it
+        view.endEditing(true)
+    }
+    
+    //MARK: - Internal methods
+    fileprivate func handleGetInfoBtnUI(enabled: Bool) {
+        getInfoBtn.isEnabled = enabled
+        getInfoBtn.alpha = enabled ? 1.0 : 0.5
+    }
+    
+    private func setupUI() {
+        /// to hide extra lines
+        intradayTableView.tableFooterView = UIView()
+        /// add left view to symbolTextField
+        symbolTextField.leftViewMode = .always
+        symbolTextField.leftView = UIView(frame: .init(x: 0, y: 0, width: 10, height: 20))
+    }
+    
+    //MARK: - Event handler methods
+    @IBAction func sortBtnClicked(_ sender: Any) {
+    
+    }
+    
+    @IBAction func getInfoBtnClicked(_ sender: Any) {
+    }
+}
+
+/// Extension for UITextFieldDelegate methods
+extension IntradayViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // get the current text, or use an empty string if that failed
+           let currentText = textField.text ?? ""
+
+        // attempt to read the range which user is trying to change, or exit if we can't
+        guard let stringRange = Range(range, in: currentText) else {
+            handleGetInfoBtnUI(enabled: false)
+            return true
+        }
+
+        // add user's new text to the existing text
+        let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        handleGetInfoBtnUI(enabled: updatedText.count > 0)
+        return true
+    }
+}
+
+/// Extension for UITableViewDatasource methods
+extension IntradayViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "IntradayTableViewCell") as? IntradayTableViewCell
+        
+        /// create new cell if it is nil
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "IntradayTableViewCell") as? IntradayTableViewCell
+        }
+        
+        return cell!
+    }
 }
